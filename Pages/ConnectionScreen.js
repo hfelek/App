9765678b626fa1e9f730d527aa53c3aa7a14c06f
hfelek <huseyin.felek@eliarge.com>
2021-12-14 10:25:38 +0300

    Settings List Created

--- Pages/ConnectionScreen.js
+++ Pages/ConnectionScreen.js
@@ -1,62 +1,21 @@
 import * as React from 'react';
-import { useState,useEffect  } from 'react';
-import { SafeAreaView,TouchableOpacity, View, FlatList, StyleSheet, Text, StatusBar } from 'react-native';
-import axios from 'axios';
-
-const LocalFixed = (a,num) => {
-  try{
-    return a.toFixed(num)
-  }
-  catch(error){
-    return a
-  }
-
-}
-
+import { SafeAreaView, View, FlatList, StyleSheet, Text, StatusBar, Settings } from 'react-native';
+const demoConnection = [{title:"Prop1",id:"id1"},{title:"Prop2",id:"id2"},{title:"Prop3",id:"id3"},{title:"Prop4",id:"id4"}]
+const Item = ({title}) => (
+  <View style={styles.item}>
+    <Text style= {styles.title}>{title}</Text> 
+  </View>
+);
 ConnectionScreen = () =>{
-  const [dummy,setDummy]= useState(0)
-  const IP = 'http://192.168.18.151/?getSensorValues'
-  const Item = ({title}) => (
-    <View style={styles.item}>
-      <Text style= {styles.title}>{title[0]}</Text> 
-      <Text style= {styles.title}>{LocalFixed(title[1],3)}</Text> 
-    </View>
-  );
-  const [time, setTime] = useState(Date.now());
-
-  useEffect(() => {
-    const interval = setInterval(() => makeRequest(), 3000);
-    return () => {
-      clearInterval(interval);
-    };
-  }, []);
-
-  async function makeRequest() {
-    try {
-      const response = await axios.get(IP);
-      //alert("You Got Response");
-      setDummy( response.data)
-
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
   const renderItem = ({item}) => (
-    <Item title = {item}/>
+    <Item title = {item.title}/>
   );
-
     return (
       <SafeAreaView style={styles.container}>
-      {/* <TouchableOpacity
-        style={styles.button}
-        onPress={makeRequest}
-      >
-        <Text>Press Here</Text>
-      </TouchableOpacity> */}
         <FlatList
-          data={Object.entries(dummy)}
+          data={demoConnection}
           renderItem={renderItem}
+          keyExtractor={item =>item.id}
         />
       </SafeAreaView>
 
@@ -65,27 +24,18 @@ ConnectionScreen = () =>{
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
     // marginTop: StatusBar.currentHeight || 0,
     marginTop:  0,
   },
   item: {
-    flexDirection:"row",
-    backgroundColor: '#ffffff',
+    backgroundColor: '#415172',
     padding: 20,
-    justifyContent:"space-between",
-    marginVertical: 1,
+    marginVertical: 8,
     marginHorizontal: 16,
   },
   title: {
-    fontSize: 14,
+    fontSize: 24,
   },
-  button: {
-    alignItems: "center",
-    backgroundColor: "#DDDDDD",
-    padding: 10
-  }
 });
 
-  export default ConnectionScreen;
-
+export default ConnectionScreen;
--- Pages/SettingsScreen.js
+++ Pages/SettingsScreen.js
@@ -1,40 +1,68 @@
 import * as React from 'react';
-import { SafeAreaView, View, FlatList, StyleSheet, Text, StatusBar, Settings } from 'react-native';
-const demoConnection = [{title:"Prop1",id:"id1"},{title:"Prop2",id:"id2"},{title:"Prop3",id:"id3"},{title:"Prop4",id:"id4"}]
+import {
+  SafeAreaView,
+  View,
+  FlatList,
+  StyleSheet,
+  Text,
+  StatusBar,
+  Settings,
+  VirtualizedList,
+} from 'react-native';
+import ICTParams from '../Pages/Objects/ICTParams.json';
+import Paramsfiltered from '../Pages/Objects/Paramsfiltered.json';
+const uniqueTags = [...new Set(ICTParams.map(item => item.Tag))];
+const a = Object.assign({}, uniqueTags);
+
+
+const demoConnection = [
+  {title: 'Prop1', id: 'id1'},
+  {title: 'Prop2', id: 'id2'},
+  {title: 'Prop3', id: 'id3'},
+  {title: 'Prop4', id: 'id4'},
+];
+
 const Item = ({title}) => (
   <View style={styles.item}>
-    <Text style= {styles.title}>{title}</Text> 
+    <Text style={styles.title}>{title}</Text>
   </View>
 );
-SettingsScreen = () =>{
-  const renderItem = ({item}) => (
-    <Item title = {item.title}/>
-  );
-    return (
-      <SafeAreaView style={styles.container}>
-        <FlatList
-          data={demoConnection}
-          renderItem={renderItem}
-          keyExtractor={item =>item.id}
-        />
-      </SafeAreaView>
 
-    );
-}
+
+
+
+SettingsScreen = () => {
+  console.log(JSON.stringify(a,null,4))
+  const renderItem = ({ item }) => (
+    <Item title={item.Tag} />
+  );
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={Paramsfiltered}
+        renderItem={renderItem}
+        keyExtractor={item =>item.Tag}
+      />
+    </SafeAreaView>
+  );
+};
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    width: '100%',
+    padding: 1,
     // marginTop: StatusBar.currentHeight || 0,
-    marginTop:  0,
+    paddingTop: 0,
   },
   item: {
-    backgroundColor: '#415172',
-    padding: 20,
+    backgroundColor: '#FFFFFF',
+    padding: 16,
     marginVertical: 8,
     marginHorizontal: 16,
   },
   title: {
-    fontSize: 24,
+    fontSize: 12,
+    color: 'black',
   },
 });
 
