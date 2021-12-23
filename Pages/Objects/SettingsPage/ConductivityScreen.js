9a61161937d85db3f85cf12564e888f14c9b3a44
hfelek <huseyin.felek@eliarge.com>
2021-12-24 00:44:25 +0300

    Concentration Sayfasına Kadar Düzeltildi

--- Pages/Objects/SettingsPage/ConductivityScreen.js
+++ Pages/Objects/SettingsPage/ConductivityScreen.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { StyleSheet, Text, View, Button, SafeAreaView, FlatList, StatusBar, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
+import { StyleSheet, Text, View, Button, SafeAreaView, FlatList, StatusBar, TouchableOpacity, ScrollView } from 'react-native'
 import Paramsfiltered from '../../Objects/Paramsfiltered.json';
 import { createStackNavigator } from '@react-navigation/stack';
 import { TextInput } from 'react-native-paper';
@@ -9,7 +9,7 @@ import react from 'react';
 import Icon from 'react-native-vector-icons/Ionicons';
 import { Slider } from "@miblanchard/react-native-slider";
 import ScrollViewNativeComponent from 'react-native/Libraries/Components/ScrollView/ScrollViewNativeComponent';
-import { or } from 'react-native-reanimated';
+import { color, or, round } from 'react-native-reanimated';
 import { RectButton } from 'react-native-gesture-handler';
 // import Slider from '@react-native-community/slider';
 //import MultiSlider from 'react-native-multi-slider';
@@ -165,42 +165,42 @@ const ConductivityScreen = ({ route, navigation }) => {
       </SafeAreaView>
     );
   };
-  const MountingFactorScreen = ({route,navigation}) => {
+  const MountingFactorScreen = ({ route, navigation }) => {
     const valSystemUnits = Values.filter(row => row.Tag == 'Conductivity');
     const val = valSystemUnits[0].menu.filter(row => row.Tag == 'Mounting Factor')[0];
     const possibleValues = val.PossibleValues;
-    const initalMFValue=val.Value;
-    const limitsMF = [possibleValues.RangeLower,possibleValues.RangeUpper]
+    const initalMFValue = val.Value;
+    const limitsMF = [possibleValues.RangeLower, possibleValues.RangeUpper]
     const [mountingFactor, setMountingFactor] = React.useState(initalMFValue);
-    function callBackSlider () {
-      if((initalMFValue!=mountingFactor)){
-     
+    function callBackSlider() {
+      if ((initalMFValue != mountingFactor)) {
+
         navigation.setOptions({
           headerRight: () => (
-          <TouchableOpacity >
-            <View style={styles.buttonBar}>
-              <Text>Save</Text>
-            </View>
-          </TouchableOpacity>
+            <TouchableOpacity >
+              <View style={styles.buttonBar}>
+                <Text>Save</Text>
+              </View>
+            </TouchableOpacity>
           ),
         });
       }
-      else{
+      else {
         navigation.setOptions({
           headerRight: () => (
             <></>
           ),
         });
       }
-    
+
     }
-    
-    
+
+
 
     return (
 
       <View style={styles.containerSlider}>
-       
+
         <Slider
           value={mountingFactor}
           onValueChange={value => setMountingFactor(value[0].toFixed(3))}
@@ -208,22 +208,160 @@ const ConductivityScreen = ({ route, navigation }) => {
           maximumValue={limitsMF[1]}
           onSlidingComplete={() => callBackSlider()}
         />
-        <Text style={{alignContent:"center"}}>Mounting Factor Value : {mountingFactor}</Text>
-        </View>
-   
+        <Text style={{ alignContent: "center" }}>Mounting Factor Value : {mountingFactor}</Text>
+      </View>
+
 
 
     );
   };
   const TemperatureCoefficientScreen = () => {
-      return(
-        <Text>Hello</Text>
-      )
+    const valSystemUnits = Values.filter(row => row.Tag == 'Conductivity');
+    const compensationVal = valSystemUnits[0].menu.filter(row => row.Tag == 'Temperature Compensation')[0].Value;
+    const coefficientMenu = valSystemUnits[0].menu.filter(row => row.Tag == 'Temperature Coefficient')[0].Menu;
+    const initialNonLinearParams = coefficientMenu.filter(row => row.Tag == 'Non-Linear')[0].Value
+    const initialLinearParams = coefficientMenu.filter(row => row.Tag == 'Linear')[0].Value[0]
+    const [nonLinearParams, setNonLinearParams] = useState(initialNonLinearParams)
+    const [linearParam, setLinearParam] = useState(initialLinearParams)
+    const [nonLinearParams1, setNonLinearParams1] = useState(nonLinearParams)
+    const [nonLinearParamT1, setNonLinearParamT1] = react.useState(initialNonLinearParams.filter(row => row.Tag == "T1")[0].Value)
+    const [nonLinearParamT2, setNonLinearParamT2] = react.useState(initialNonLinearParams.filter(row => row.Tag == "T2")[0].Value)
+    const [nonLinearParamT3, setNonLinearParamT3] = react.useState(initialNonLinearParams.filter(row => row.Tag == "T3")[0].Value)
+    const [nonLinearParamT4, setNonLinearParamT4] = react.useState(initialNonLinearParams.filter(row => row.Tag == "T4")[0].Value)
+    const [nonLinearParamT5, setNonLinearParamT5] = react.useState(initialNonLinearParams.filter(row => row.Tag == "T5")[0].Value)
+    const [nonLinearParamC1, setNonLinearParamC1] = react.useState(initialNonLinearParams.filter(row => row.Tag == "C1")[0].Value)
+    const [nonLinearParamC2, setNonLinearParamC2] = react.useState(initialNonLinearParams.filter(row => row.Tag == "C2")[0].Value)
+    const [nonLinearParamC3, setNonLinearParamC3] = react.useState(initialNonLinearParams.filter(row => row.Tag == "C3")[0].Value)
+    const [nonLinearParamC4, setNonLinearParamC4] = react.useState(initialNonLinearParams.filter(row => row.Tag == "C4")[0].Value)
+    const [nonLinearParamC5, setNonLinearParamC5] = react.useState(initialNonLinearParams.filter(row => row.Tag == "C5")[0].Value)
+    console.log(nonLinearParamT1)
+    console.log()
+    return (
+      // <ReturnMenu param={compensationVal} />
+      <SafeAreaView style={[styles.container,{ flex: 1, flexDirection: "row" }]}>
+        {/* <ScrollView style={{ flex: 1, flexDirection: "row" }}> */}
+          <View style={{ flex: 1,paddingTop:5}}>
+            <Text style={styles.basicText}>  T1 Param  </Text>
+            <TextInput style={styles.input}
+              value={nonLinearParamT1}
+              // placeholder={nonLinearParamT1}
+              onChangeText={(text) => (setNonLinearParamT1(text))}
+              // onBlur={(text) =>handleTextChangeEnd(text,item)}
+              maxLength={5}
+              editable
+
+              keyboardType="numeric"
+            />
+            <Text style={styles.basicText}>  T2 Param  </Text>
+            <TextInput style={styles.input}
+              value={nonLinearParamT2}
+              // placeholder={nonLinearParamT1}
+              onChangeText={(text) => (setNonLinearParamT2(text))}
+              // onBlur={(text) =>handleTextChangeEnd(text,item)}
+              maxLength={5}
+              editable
+
+              keyboardType="numeric"
+            />
+            <Text style={styles.basicText}>  T3 Param  </Text>
+            <TextInput style={styles.input}
+              value={nonLinearParamT3}
+              // placeholder={nonLinearParamT1}
+              onChangeText={(text) => (setNonLinearParamT3(text))}
+              // onBlur={(text) =>handleTextChangeEnd(text,item)}
+              maxLength={5}
+              editable
+
+              keyboardType="numeric"
+            />
+            <Text style={styles.basicText}>  T4 Param  </Text>
+            <TextInput style={styles.input}
+              value={nonLinearParamT4}
+              // placeholder={nonLinearParamT1}
+              onChangeText={(text) => (setNonLinearParamT4(text))}
+              // onBlur={(text) =>handleTextChangeEnd(text,item)}
+              maxLength={5}
+              editable
+
+              keyboardType="numeric"
+            />
+            <Text style={styles.basicText}>  T5 Param  </Text>
+            <TextInput style={styles.input}
+              value={nonLinearParamT5}
+              // placeholder={nonLinearParamT1}
+              onChangeText={(text) => (setNonLinearParamT5(text))}
+              // onBlur={(text) =>handleTextChangeEnd(text,item)}
+              maxLength={5}
+              editable
+
+              keyboardType="numeric"
+            />
+          </View>
+          <View style={{ flex: 1, paddingTop:5 }}>
+            <Text style={styles.basicText}>  C1 Param  </Text>
+            <TextInput style={styles.input}
+              value={nonLinearParamC1}
+              // placeholder={nonLinearParamT1}
+              onChangeText={(text) => (setNonLinearParamC1(text))}
+              // onBlur={(text) =>handleTextChangeEnd(text,item)}
+              maxLength={5}
+              editable
+
+              keyboardType="numeric"
+            />
+            <Text style={styles.basicText}>  C2 Param  </Text>
+            <TextInput style={styles.input}
+              value={nonLinearParamC2}
+              // placeholder={nonLinearParamT1}
+              onChangeText={(text) => (setNonLinearParamC2(text))}
+              // onBlur={(text) =>handleTextChangeEnd(text,item)}
+              maxLength={5}
+              editable
+
+              keyboardType="numeric"
+            />
+            <Text style={styles.basicText}>  C3 Param  </Text>
+            <TextInput style={styles.input}
+              value={nonLinearParamC3}
+              // placeholder={nonLinearParamT1}
+              onChangeText={(text) => (setNonLinearParamC3(text))}
+              // onBlur={(text) =>handleTextChangeEnd(text,item)}
+              maxLength={5}
+              editable
+
+              keyboardType="numeric"
+            />
+            <Text style={styles.basicText}>  C4 Param  </Text>
+            <TextInput style={styles.input}
+              value={nonLinearParamC4}
+              // placeholder={nonLinearParamT1}
+              onChangeText={(text) => (setNonLinearParamC4(text))}
+              // onBlur={(text) =>handleTextChangeEnd(text,item)}
+              maxLength={5}
+              editable
+
+              keyboardType="numeric"
+            />
+            <Text style={styles.basicText}>  C5 Param  </Text>
+            <TextInput style={styles.input}
+              value={nonLinearParamC5}
+              // placeholder={nonLinearParamT1}
+              onChangeText={(text) => (setNonLinearParamC5s(text))}
+              // onBlur={(text) =>handleTextChangeEnd(text,item)}
+              maxLength={5}
+              editable
+
+              keyboardType="numeric"
+            />
+          </View>
+        {/* </ScrollView> */}
+      </SafeAreaView>
+    )
   };
   const TemperatureCompensationScreen = ({ route, navigation }) => {
     const valSystemUnits = Values.filter(row => row.Tag == 'Conductivity');
     const val = valSystemUnits[0].menu.filter(row => row.Tag == 'Temperature Compensation');
-    
+
     const possibleValues = val[0].Menu;
     const [selection, setSelection] = React.useState(val[0].Value);
     function ItemSelectable(title) {
@@ -269,38 +407,38 @@ const ConductivityScreen = ({ route, navigation }) => {
     const valSystemUnits = Values.filter(row => row.Tag == 'Conductivity');
     const val = valSystemUnits[0].menu.filter(row => row.Tag == 'Reference Temperature');
     const possibleValues = val[0].PossibleValues;
-    const initialValC=possibleValues.filter(row => row.Tag == '°C')[0].Value
-    const initialValF=possibleValues.filter(row => row.Tag == '°F')[0].Value
+    const initialValC = possibleValues.filter(row => row.Tag == '°C')[0].Value
+    const initialValF = possibleValues.filter(row => row.Tag == '°F')[0].Value
 
     const [temperatureC, setTemperatureC] = React.useState(initialValC);
     const [temperatureF, setTemperatureF] = React.useState(initialValF);
-    const limitsF = [possibleValues.filter(row => row.Tag == '°F')[0].RangeLower,possibleValues.filter(row => row.Tag == '°F')[0].RangeUpper]
-    const limitsC = [possibleValues.filter(row => row.Tag == '°C')[0].RangeLower,possibleValues.filter(row => row.Tag == '°C')[0].RangeUpper]
-    function callBackSlider () {
-    if((temperatureF!=initialValF) ||(temperatureC!=initialValC)){
-      console.log({temperatureF,initialValF,temperatureC,initialValC})
-      navigation.setOptions({
-        headerRight: () => (
-        <TouchableOpacity >
-          <View style={styles.buttonBar}>
-            <Text>Save</Text>
-          </View>
-        </TouchableOpacity>
-        ),
-      });
+    const limitsF = [possibleValues.filter(row => row.Tag == '°F')[0].RangeLower, possibleValues.filter(row => row.Tag == '°F')[0].RangeUpper]
+    const limitsC = [possibleValues.filter(row => row.Tag == '°C')[0].RangeLower, possibleValues.filter(row => row.Tag == '°C')[0].RangeUpper]
+    function callBackSlider() {
+      if ((temperatureF != initialValF) || (temperatureC != initialValC)) {
+        console.log({ temperatureF, initialValF, temperatureC, initialValC })
+        navigation.setOptions({
+          headerRight: () => (
+            <TouchableOpacity >
+              <View style={styles.buttonBar}>
+                <Text>Save</Text>
+              </View>
+            </TouchableOpacity>
+          ),
+        });
+      }
+      else {
+        navigation.setOptions({
+          headerRight: () => (
+            <></>
+          ),
+        });
+      }
     }
-    else{
-      navigation.setOptions({
-        headerRight: () => (
-          <></>
-        ),
-      });
-    }
-  }
     return (
 
       <View style={styles.containerSlider}>
-       
+
         <Slider
           value={temperatureC}
           onValueChange={value => setTemperatureC(value[0].toFixed(1))}
@@ -308,7 +446,7 @@ const ConductivityScreen = ({ route, navigation }) => {
           maximumValue={limitsC[1]}
           onSlidingComplete={() => callBackSlider()}
         />
-        <Text style={{alignContent:"center"}}>°C Value : {temperatureC}</Text>
+        <Text style={{ alignContent: "center" }}>°C Value : {temperatureC}</Text>
         <Slider
           value={temperatureF}
           onValueChange={value => setTemperatureF(value[0].toFixed(1))}
@@ -316,10 +454,10 @@ const ConductivityScreen = ({ route, navigation }) => {
           maximumValue={limitsF[1]}
           onSlidingComplete={() => callBackSlider()}
         />
-        <Text style={{alignContent:"center"}}>°F Value  : {temperatureF}</Text>
-      
-        </View>
-   
+        <Text style={{ alignContent: "center" }}>°F Value  : {temperatureF}</Text>
+
+      </View>
+
 
 
     );
@@ -329,39 +467,39 @@ const ConductivityScreen = ({ route, navigation }) => {
 
   };
 
-  const FilterCountConstantScreen = ({route,navigation}) => {
+  const FilterCountConstantScreen = ({ route, navigation }) => {
     const valSystemUnits = Values.filter(row => row.Tag == 'Conductivity');
     const val = valSystemUnits[0].menu.filter(row => row.Tag == 'Filter Count Constant')[0];
     const possibleValues = val.PossibleValues;
-    const initalFCCValue=val.Value;
-    const limitsFFC = [possibleValues.RangeLower,possibleValues.RangeUpper]
+    const initalFCCValue = val.Value;
+    const limitsFFC = [possibleValues.RangeLower, possibleValues.RangeUpper]
     const [filterCC, setFilterCC] = React.useState(initalFCCValue);
-    function callBackSlider () {
-      if((initalFCCValue!=filterCC)){
-     
+    function callBackSlider() {
+      if ((initalFCCValue != filterCC)) {
+
         navigation.setOptions({
           headerRight: () => (
-          <TouchableOpacity >
-            <View style={styles.buttonBar}>
-              <Text>Save</Text>
-            </View>
-          </TouchableOpacity>
+            <TouchableOpacity >
+              <View style={styles.buttonBar}>
+                <Text>Save</Text>
+              </View>
+            </TouchableOpacity>
           ),
         });
       }
-      else{
+      else {
         navigation.setOptions({
           headerRight: () => (
             <></>
           ),
         });
       }
-    
+
     }
     return (
 
       <View style={styles.containerSlider}>
-       
+
         <Slider
           value={filterCC}
           onValueChange={value => setFilterCC(value[0].toFixed(0))}
@@ -369,28 +507,58 @@ const ConductivityScreen = ({ route, navigation }) => {
           maximumValue={limitsFFC[1]}
           onSlidingComplete={() => callBackSlider()}
         />
-        <Text style={{alignContent:"center"}}>Filter Count Constant: {filterCC}</Text>
-        </View>
-   
+        <Text style={{ alignContent: "center" }}>Filter Count Constant: {filterCC}</Text>
+      </View>
+
 
 
     );
   };
-  const ZeroPointScreeen = () => {
+  const ZeroPointScreeen = ({route,navigation}) => {
     const valSystemUnits = Values.filter(row => row.Tag == 'Conductivity');
     const val = valSystemUnits[0].menu.filter(row => row.Tag == 'Zero Point');
-    const possibleValues = val[0].PossibleValues;
-    const [selection, setSelection] = React.useState(val[0].Value);
-    console.log(possibleValues)
-    console.log(typeof (possibleValues))
+    const possibleValues = val[0].Menu;
+    const rangeValue =valSystemUnits[0].menu.filter(row => row.Tag == 'Range')[0].Value
+    const itemToBeRenderedInitial = possibleValues.filter(row => row.Tag==rangeValue)[0]
+
+    const [selection, setSelection] = React.useState(Number(itemToBeRenderedInitial.Value));
+    console.log(Number(itemToBeRenderedInitial.Stepsize))
+    
+    function callBackSlider() {
+      if ((selection != Number(itemToBeRenderedInitial.Value))) {
+        navigation.setOptions({
+          headerRight: () => (
+            <TouchableOpacity >
+              <View style={styles.buttonBar}>
+                <Text>Save</Text>
+              </View>
+            </TouchableOpacity>
+          ),
+        });
+      }
+      else {
+        navigation.setOptions({
+          headerRight: () => (
+            <></>
+          ),
+        });
+      }
+    }
     return (
-      <SafeAreaView style={styles.container}>
-        <FlatList
-          data={possibleValues}
-          renderItem={renderItem1}
-          keyExtractor={item => item.Tag}
+      <View style={styles.containerSlider}>
+
+        <Slider
+          value={Number(selection)}
+          onValueChange={value => setSelection((Math.round(value*100)/100))}
+          minimumValue={Number(itemToBeRenderedInitial.RangeLower)}
+          maximumValue={Number(itemToBeRenderedInitial.RangeUpper)}
+          onSlidingComplete={() => callBackSlider()}
+          step={Number(itemToBeRenderedInitial.Stepsize)}
         />
-      </SafeAreaView>
+        <Text style={{ alignContent: "center" }}>Zero Point Value: {selection}</Text>
+        <Text style={{ alignContent: "center" }}>Measurement Range: {rangeValue}</Text>
+
+      </View>
     );
   };
 
@@ -437,6 +605,7 @@ const styles = StyleSheet.create({
     borderBottomWidth: StyleSheet.hairlineWidth,
 
   },
+  basicText:{color:"#000",textAlign:"center"},
   title: {
     fontSize: 15,
     color: 'black',
@@ -467,8 +636,14 @@ const styles = StyleSheet.create({
     fontSize: 25,
     textAlign: 'center'
   },
+  input: {
+    margin: 15,
+    height: 40,
+    borderColor: '#7a42f4',
+    borderWidth: 1
+  },
   containerSlider: {
-    flex:1,
+    flex: 1,
     marginLeft: 30,
     marginRight: 30,
     alignItems: 'stretch',
